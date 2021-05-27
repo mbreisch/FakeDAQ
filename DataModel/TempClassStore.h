717b2fa61e1ac0f75f1ab3888b7264946d5ba3b2
mbreisch <64076923+mbreisch@users.noreply.github.com>
2021-05-27 11:54:59 +0200

    Add files via upload

---
+++ DataModel/TempClassStore.h
@@ -0,0 +1,35 @@
+#ifndef TempClassStore_H
+#define TempClassStore_H
+
+#include <iostream>
+#include <vector>
+#include <map>
+
+#include <PsecData.h> 
+
+using namespace std;
+
+class TempClassStore{
+
+
+ public:
+
+  TempClassStore();
+
+  vector<string> deltaT;
+ 
+  int receiveFlag=0;
+
+  PsecData *pdata = new PsecData;
+
+  std::map<int, PsecData> PsecClassStore;
+
+  std::map<int, vector<unsigned short>> ParsedDataStream; // channelnumber|data
+  std::vector<unsigned short> ParsedMetaStream; //meta
+
+ private:
+
+ 
+};
+
+#endif
