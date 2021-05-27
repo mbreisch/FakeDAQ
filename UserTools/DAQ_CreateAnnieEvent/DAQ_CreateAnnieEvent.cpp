a1a3cfbbca46a33c7df4fb2b1cd49e5c0ccb560e
mbreisch <64076923+mbreisch@users.noreply.github.com>
2021-05-27 14:42:29 +0200

    Update DAQ_CreateAnnieEvent.cpp

--- UserTools/DAQ_CreateAnnieEvent/DAQ_CreateAnnieEvent.cpp
+++ UserTools/DAQ_CreateAnnieEvent/DAQ_CreateAnnieEvent.cpp
@@ -36,20 +36,23 @@ bool DAQ_CreateAnnieEvent::Execute(){
 		VecTmpWave.push_back(tmpWave);
 		LAPPDWaveforms.insert(LAPPDWaveforms.end(),std::pair<unsigned long, vector<Waveform<double>>>((unsigned long)it->first,VecTmpWave));
 	}	
-	std::cout << "6" << std::endl;	
-	m_data->Stores["ANNIEEvent"]->Set(OutputWavLabel,LAPPDWaveforms);
+	  
+	if(LAPPDWaveforms.size()>500 || dt>1)
+	{
+		std::cout << "6" << std::endl;	
+		m_data->Stores["ANNIEEvent"]->Set(OutputWavLabel,LAPPDWaveforms);
 		std::cout << "7" << std::endl;	
-	m_data->Stores["ANNIEEvent"]->Set("ACDCmetadata",m_data->TCS.ParsedMetaStream);
+		m_data->Stores["ANNIEEvent"]->Set("ACDCmetadata",m_data->TCS.ParsedMetaStream);
 		std::cout << "8" << std::endl;	
-  	m_data->Stores["ANNIEEvent"]->Save(path);
-  		std::cout << "9" << std::endl;	
-  	m_data->Stores["ANNIEEvent"]->Delete();
+		m_data->Stores["ANNIEEvent"]->Save(path);
+		std::cout << "9" << std::endl;	
+  	}
   }
 	return true;
 }
 
 
 bool DAQ_CreateAnnieEvent::Finalise(){
-
+  m_data->Stores["ANNIEEvent"]->Delete();
   return true;
 }
