e8fbd8bf6c6500710bfc8058742dff3b5d08ced5
mbreisch <64076923+mbreisch@users.noreply.github.com>
2021-05-31 18:12:41 +0200

    Update DAQ_CreateAnnieEvent.cpp

--- UserTools/DAQ_CreateAnnieEvent/DAQ_CreateAnnieEvent.cpp
+++ UserTools/DAQ_CreateAnnieEvent/DAQ_CreateAnnieEvent.cpp
@@ -23,29 +23,29 @@ bool DAQ_CreateAnnieEvent::Initialise(std::string configfile, DataModel &data){
 
 bool DAQ_CreateAnnieEvent::Execute(){
 
-  if(m_data->TCS.receiveFlag == 1)
-  {
-	std::map<unsigned long, vector<Waveform<double>>> LAPPDWaveforms;
-	Waveform<double> tmpWave;
-	vector<Waveform<double>> VecTmpWave;
-	for(std::map<int, vector<unsigned short>>::iterator it=m_data->TCS.ParsedDataStream.begin(); it!=m_data->TCS.ParsedDataStream.end(); ++it)
+	if(m_data->TCS.receiveFlag == 1)
 	{
-		for(unsigned short k: it->second)
+		std::map<unsigned long, vector<Waveform<double>>> LAPPDWaveforms;
+		Waveform<double> tmpWave;
+		vector<Waveform<double>> VecTmpWave;
+		for(std::map<int, vector<unsigned short>>::iterator it=m_data->TCS.ParsedDataStream.begin(); it!=m_data->TCS.ParsedDataStream.end(); ++it)
 		{
-			tmpWave.PushSample((double)k);
-		}
-		VecTmpWave.push_back(tmpWave);
-		LAPPDWaveforms.insert(LAPPDWaveforms.end(),std::pair<unsigned long, vector<Waveform<double>>>((unsigned long)it->first,VecTmpWave));
-	}	
-	  
-	m_data->Stores["ANNIEEvent"]->Set(OutputWavLabel,LAPPDWaveforms);
-	m_data->Stores["ANNIEEvent"]->Set("ACDCmetadata",m_data->TCS.ParsedMetaStream);
-	m_data->Stores["ANNIEEvent"]->Save(path.c_str());
-	std::cout << "SAVED" << std::endl;	
-	m_data->Stores["ANNIEEvent"]->Delete();
-	m_data->TCS.ParsedDataStream.clear();
-	m_data->TCS.ParsedMetaStream.clear();
-  }
+			for(unsigned short k: it->second)
+			{
+				tmpWave.PushSample((double)k);
+			}
+			VecTmpWave.push_back(tmpWave);
+			LAPPDWaveforms.insert(LAPPDWaveforms.end(),std::pair<unsigned long, vector<Waveform<double>>>((unsigned long)it->first,VecTmpWave));
+		}	
+
+		m_data->Stores["ANNIEEvent"]->Set(OutputWavLabel,LAPPDWaveforms);
+		m_data->Stores["ANNIEEvent"]->Set("ACDCmetadata",m_data->TCS.ParsedMetaStream);
+		m_data->Stores["ANNIEEvent"]->Save(path.c_str());
+		std::cout << "SAVED" << std::endl;	
+		m_data->Stores["ANNIEEvent"]->Delete();
+		m_data->TCS.ParsedDataStream.clear();
+		m_data->TCS.ParsedMetaStream.clear();
+	}
 	return true;
 }
 
