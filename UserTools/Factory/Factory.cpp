ca6bdaf53dd348e2c04d65ec24a78bd8406e6cb1
mbreisch <64076923+mbreisch@users.noreply.github.com>
2021-05-31 09:28:52 -0700

    Update Factory.cpp

--- UserTools/Factory/Factory.cpp
+++ UserTools/Factory/Factory.cpp
@@ -11,6 +11,7 @@ if (tool=="DAQ_ParseData") ret=new DAQ_ParseData;
 if (tool=="DAQ_ScConfig") ret=new DAQ_ScConfig;
 if (tool=="DAQ_SCMonitorReceive") ret=new DAQ_SCMonitorReceive;
 if (tool=="DummyTool") ret=new DummyTool;
-  if (tool=="DAQ_LoadGeometry") ret=new DAQ_LoadGeometry;
+if (tool=="DAQ_LoadGeometry") ret=new DAQ_LoadGeometry;
+  if (tool=="PrintParsedData") ret=new PrintParsedData;
 return ret;
 }
