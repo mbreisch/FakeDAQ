17c0615e328fda6706b5406445916fc4d5dc04b0
mbreisch <64076923+mbreisch@users.noreply.github.com>
2021-06-24 14:34:44 +0200

    Update DAQ_CreateAnnieEvent.cpp

--- UserTools/DAQ_CreateAnnieEvent/DAQ_CreateAnnieEvent.cpp
+++ UserTools/DAQ_CreateAnnieEvent/DAQ_CreateAnnieEvent.cpp
@@ -61,7 +61,7 @@ bool DAQ_CreateAnnieEvent::Execute(){
 		//m_data->Stores["LAPPDStore"]->Set(PPSLabel,m_data->TCS.ParsedPpsStream);
 		m_data->Stores["LAPPDStore"]->Save(path.c_str()); std::cout << "SAVED" << std::endl;	
 		m_data->Stores["LAPPDStore"]->Delete();
-		m_data->Stores["LAPPDStore"]->Header->Get("TotalEntries",entries);	
+		std::cout << m_data->Stores.at("LAPPDStore")->Header->Get("TotalEntries",entries) << "?" << std:endl; 	 
 		
 
 	}else
