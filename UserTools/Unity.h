a4877bbf2bc5b1693dd0469a9fd854cddcb7a0d0
mbreisch <64076923+mbreisch@users.noreply.github.com>
2022-01-11 13:51:30 +0100

    Update Unity.h

--- UserTools/Unity.h
+++ UserTools/Unity.h
@@ -1,9 +1,5 @@
 #include <DAQ_AccConfig.h>
 #include <DAQ_AccDataReceive.h>
 #include <DAQ_CreateAnnieEvent.h>
-#include <DAQ_ParseData.h>
-#include <DAQ_ScConfig.h>
-#include <DAQ_SCMonitorReceive.h>
 #include <DummyTool.h>
-#include "DAQ_LoadGeometry.h"
-#include <PrintParsedData.h>
+
