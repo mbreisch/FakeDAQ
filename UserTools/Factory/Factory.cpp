0ca791128c4a012165148112263fedcb0b9872c3
mbreisch <64076923+mbreisch@users.noreply.github.com>
2022-01-11 13:51:48 +0100

    Update Factory.cpp

--- UserTools/Factory/Factory.cpp
+++ UserTools/Factory/Factory.cpp
@@ -7,11 +7,7 @@ Tool* ret=0;
 if (tool=="DAQ_AccConfig") ret=new DAQ_AccConfig;
 if (tool=="DAQ_AccDataReceive") ret=new DAQ_AccDataReceive;
 if (tool=="DAQ_CreateAnnieEvent") ret=new DAQ_CreateAnnieEvent;
-if (tool=="DAQ_ParseData") ret=new DAQ_ParseData;
-if (tool=="DAQ_ScConfig") ret=new DAQ_ScConfig;
-if (tool=="DAQ_SCMonitorReceive") ret=new DAQ_SCMonitorReceive;
 if (tool=="DummyTool") ret=new DummyTool;
-if (tool=="DAQ_LoadGeometry") ret=new DAQ_LoadGeometry;
-  if (tool=="PrintParsedData") ret=new PrintParsedData;
+
 return ret;
 }
